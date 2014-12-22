9cbab30d66aaa33699726bd3989bebe99af86e40
René Fleschenberg <rene@fleschenberg.net>
2014-12-22 14:17:58 +0100

    Don't store customer information
    The implementation of this feature relied on a deprecated Django API.
    As far as I see, it also relied on a a model field being present on the user
    profile, but this was not documented anywhere.
    Maybe we can re-implement this feature at a later point.

--- shop_stripe/offsite_stripe.py
+++ shop_stripe/offsite_stripe.py
@@ -22,8 +22,6 @@ class StripeBackend(object):
     A django-shop payment backend for the stripe service, this
     is the workhorse view. It processes what the CardForm class
     kicks back to the server.
-
-    It also saves the customer billing information for later use.
     """
     backend_name = "Stripe"
     url_namespace = "stripe"
@@ -44,12 +42,7 @@ class StripeBackend(object):
 
     def stripe_payment_view(self, request):
         if request.POST:
-            if (request.user.is_authenticated() and
-                    request.user.get_profile().stripe_customer_id):
-                customer_id = request.user.get_profile().stripe_customer_id
-            else:
-                customer_id = None
-                card_token = request.POST['stripeToken']
+            card_token = request.POST['stripeToken']
             order = self.shop.get_order(request)
             order_id = self.shop.get_order_unique_id(order)
             amount = self.shop.get_order_total(order)
@@ -70,32 +63,15 @@ class StripeBackend(object):
                     'in your configuration file.'
                 )
 
-            if not customer_id:
-                stripe_dict = {
-                    'amount': amount,
-                    'currency': currency,
-                    'card': card_token,
-                    'description': description, }
-            else:
-                stripe_dict = {
-                    'amount': amount,
-                    'currency': currency,
-                    'customer': customer_id,
-                    'description': description, }
+            stripe_dict = {
+                'amount': amount,
+                'currency': currency,
+                'card': card_token,
+                'description': description, }
 
             stripe_result = stripe.Charge.create(**stripe_dict)
             self.shop.confirm_payment(self.shop.get_order_for_id(
                 order_id), amount, stripe_result['id'], self.backend_name)
-
-            # If we're logged in, save the transaction token for use again
-            # later. Sweet.
-            if (request.user.is_authenticated() and not
-                    request.user.get_profile().stripe_customer_id):
-                customer = stripe.Customer.create(
-                    card=card_token, description=description)
-                profile = request.user.get_profile()
-                profile.stripe_customer_id = customer.id
-                profile.save()
 
         if hasattr(settings, 'SHOP_STRIPE_PUBLISHABLE_KEY'):
             pub_key = settings.SHOP_STRIPE_PUBLISHABLE_KEY
